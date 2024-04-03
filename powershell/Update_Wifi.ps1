e3390f47d022bc1c850c390cf2e3d2b628b9bb90
David Rush <drush@evertz.com>
2024-04-03 11:51:48 -0400

    Changes to be committed: 	modified:   powershell/Update_Wifi.ps1

--- powershell/Update_Wifi.ps1
+++ powershell/Update_Wifi.ps1
@@ -1,1 +1,1 @@
-pnputil.exe /add-driver "C:\Administrator\Desktop\Drivers\v23.30.0\*.inf" /install > "C:\Administrator\Desktop\Drivers\output.txt"
+pnputil.exe /add-driver "C:\Users\Administrator\Desktop\Drivers\v23.30.0\*.inf" /install > "C:\Users\Administrator\Desktop\Drivers\output.txt"
