9d1905212e1228988db79a561705032b070b619e
David Rush <drush@evertz.com>
2024-04-03 11:55:53 -0400

    Changes to be committed: 	deleted:    Update_Wifi.ps1 	new file:   Update_WifiCard.ps1 	deleted:    Update_WifiDriver.ps1

--- powershell/Update_Wifi.ps1
+++
@@ -1,1 +0,0 @@
-pnputil.exe /add-driver "C:\Users\Administrator\Desktop\Drivers\v23.30.0\*.inf" /install > "C:\Users\Administrator\Desktop\Drivers\output.txt"
---
+++ powershell/Update_WifiCard.ps1
@@ -0,0 +1,1 @@
+pnputil.exe /add-driver "C:\Users\Administrator\Desktop\v23.30.0\*.inf" /install > "C:\Users\Administrator\Desktop\output.txt"
