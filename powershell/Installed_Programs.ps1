78de46c9efe7f3809ed3326c6c85abd8a8b6d2e3
David Rush <drush@evertz.com>
2024-04-05 09:05:24 -0400

    Changes to be committed: 	modified:   Installed_Programs.ps1

--- powershell/Installed_Programs.ps1
+++ powershell/Installed_Programs.ps1
@@ -15,11 +15,10 @@ $s += Analyze 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' 64
 $s += Analyze 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' 32
 $s | Sort-Object -Property Name > .\Output.txt
 
-$attachments = @()
-$attachments += .\Output.txt
+$Output = Get-Content .\Output.txt -Raw
 $compsys = Get-WmiObject win32_computersystem
 $mail = "mail." + $compsys.Domain
+$Body = $compsys.DnsHostname + "`n`n" + $Output
 
-
-Send-MailMessage -SmtpServer $mail -To "drush@evertz.com" -From "Reports@5288.IT" -Body $compsys.DnsHostname -Subject "Installed Programs" -attachments $attachments
+Send-MailMessage -SmtpServer $mail -To "drush@evertz.com" -From "Reports@5288.IT" -Body $Body -Subject "Installed Programs"
 Remove-Item .\Output.txt -Force
