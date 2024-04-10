7a7232bf1f55fd5fffb916b223260b8582ad4131
David Rush <drush@evertz.com>
2024-04-10 12:00:11 -0400

    Changes to be committed: 	new file:   Check_WifiCard.ps1

---
+++ powershell/Check_WifiCard.ps1
@@ -0,0 +1,11 @@
+$netshinfo = netsh wlan sh d
+
+$hostname = hostname
+
+$netshinfo > C:\output.txt
+
+$netsh = netsh wlan sh int
+
+$netsh >> C:\output.txt
+
+Send-MailMessage -SmtpServer mail -To drush@evertz.com -From Reports@5288.IT -Attachments C:\output.txt -Subject WifiCardCheck -Body $hostname
