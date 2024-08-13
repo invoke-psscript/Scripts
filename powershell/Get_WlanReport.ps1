5710cc8e876d0a81a301e327dfffc7470f4027d4
David Rush <drush@evertz.com>
2024-08-13 09:41:27 -0400

    Changes to be committed: 	new file:   Get_WlanReport.ps1

---
+++ powershell/Get_WlanReport.ps1
@@ -0,0 +1,23 @@
+$attachments = @()
+
+$netsh_devices = netsh wlan sh d
+
+$hostname = hostname
+
+$netsh_devices > C:\output.txt
+
+$netsh_int = netsh wlan sh int
+
+$netsh_int >> C:\output.txt
+
+$netsh_all = netsh wlan sh all
+
+$netsh_all >> C:\output.txt
+
+$attachments += "C:\output.txt"
+
+netsh wlan sh wlanreport
+
+$attachments += "C:\ProgramData\Microsoft\Windows\WlanReport\wlan-report-latest.html"
+
+Send-MailMessage -SmtpServer mail -To drush@evertz.com -From Reports@5288.IT -Attachments $attachments -Subject WifiCardReport -Body $hostname
