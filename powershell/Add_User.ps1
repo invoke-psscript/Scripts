158d5a9609da8f9edf0f9acf79499f2b719b3ba8
David Rush <drush@evertz.com>
2024-08-30 10:30:07 -0400

    Changes to be committed: 	modified:   Add_User.ps1

--- powershell/Add_User.ps1
+++ powershell/Add_User.ps1
@@ -1,1 +1,36 @@
-Add-LocalGroupMember -Group "Administrators" -Member "EVERTZ_MICROSYS\drush"
+Write-Host "Note: Username must be entered in the form DOMAIN\username in the Command Line section of the script`n----------`n`n"
+
+$username = $args[0]
+
+If ($username.Length -gt 16){
+
+    If ($username -ne $null){
+
+        If ($username -in (Get-LocalGroupMember "Administrators").Name){
+
+            Write-Host "$username is already a member of the local Administrators group"
+
+        }
+        
+        Else {
+
+            Write-Host "Adding $username to the local Administrators group"
+            Add-LocalGroupMember -Group "Administrators" -Member "$username"
+
+        }
+
+    }
+
+    Else {
+
+        Write-Host "No username entered"
+        
+        }
+
+}
+
+Else { 
+
+    Write-Host "Double-check your username input - Username MUST be entered in the form DOMAIN\username in the Command Line section of the script"
+
+}
