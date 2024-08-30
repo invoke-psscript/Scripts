26c29f84c4a9203642a7667763b32d8ef3eacafc
David Rush <drush@evertz.com>
2024-08-30 09:50:38 -0400

    Changes to be committed: 	modified:   Add_RDP.ps1

--- powershell/Add_RDP.ps1
+++ powershell/Add_RDP.ps1
@@ -1,22 +1,36 @@
 Write-Host "Note: Username must be entered in the form DOMAIN\username in the Command Line section of the script`n----------`n`n"
 
-If ($args[0] -ne $null){
+$username = $args[0]
 
-    $username = $args[0]
+If ($username.Length -gt 16){
 
-    If ($username -in (Get-LocalGroupMember "Remote Desktop Users").Name){
+    If ($username -ne $null){
 
-        Write-Host "$username is already a member of the local Remote Desktop Users group"
+        If ($username -in (Get-LocalGroupMember "Remote Desktop Users").Name){
+
+            Write-Host "$username is already a member of the local Remote Desktop Users group"
+
+        }
+        
+        Else {
+
+            Write-Host "Adding $username to the local Remote Desktop Users group"
+            Add-LocalGroupMember -Group "Remote Desktop Users" -Member "$username"
+
+        }
 
     }
-    
+
     Else {
 
-        Write-Host "Adding $username to the local Remote Desktop Users group"
-        Add-LocalGroupMember -Group "Remote Desktop Users" -Member "$username"
-
-    }
+        Write-Host "No username entered"
+        
+        }
 
 }
 
-Else {Write-Host "No username entered"}
+Else { 
+
+    Write-Host "Double-check your username input - Username MUST be entered in the form DOMAIN\username in the Command Line section of the script"
+
+}
